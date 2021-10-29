fd75f53d42f2ec514f8819a03c5e07e4c5133738
ElenaRozenkova <elena.rozenkova@gmail.com>
2021-10-29 12:23:07 +0300

    создали функцию dowbleElem

---
+++ js/funcions.js
@@ -0,0 +1,9 @@
+function dowbleElem() {
+    let arr = [1, 2, 3];
+    const dowbleArr = arr.map(function (dowble) {
+      return dowble * 2;
+    });
+    console.log(dowbleArr);
+  }
+  
+  dowbleElem();
