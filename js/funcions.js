31e9b5255947889832a399a2ca31c6052e6b00a6
ElenaRozenkova <elena.rozenkova@gmail.com>
2021-11-01 11:38:44 +0200

    доработана функция

--- js/funcions.js
+++ js/funcions.js
@@ -1,8 +1,41 @@
 
-  function dowbleElem(ar) {
+
+
+function dowbleElem(ar) {
   let arr = ar;
-  const dowbleArr = arr.map(function (item) {
-    return item * 2;
-  });
-console.log(dowbleArr);
+  if ( Array.isArray(arr)) {
+    const dowbleArr = arr.map(function (item) {
+      return item * 2;
+    });
+    console.log(dowbleArr);
+  } else {
+  alert('Не массив');
+  }
+  
+
 };
+
+// function dowbleElem(ar) {
+//   let arr = ar;
+//   if ( Array.isArray(arr)) {
+//     for(let i=0; i<=arr.length;i++){
+//       if(arr[i]===Number){
+//         const dowbleArr = arr.map(function (item) {
+//           return item * 2;
+//         });
+//         console.log(dowbleArr);
+//       } else {
+        
+//         alert('Массив содержит не числовое значени');
+//         break;
+
+//       }
+
+//     }
+    
+//   } else {
+//   alert('Не массив');
+//   }
+  
+
+// };
