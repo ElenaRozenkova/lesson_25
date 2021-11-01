9bc1c6b27cdfe135138e26fa4d04877777d438ba
ElenaRozenkova <elena.rozenkova@gmail.com>
2021-11-01 11:39:34 +0200

    проверка массива

--- js/test.js
+++ js/test.js
@@ -1,14 +1,27 @@
-describe(`тестирование функции возведения в степень`,()=>{
-    it(`возведение 2 в степень 3`,()=>{
-        assert.equal(8,8);
-    });
-});
+// describe(`тестирование функции возведения в степень`,()=>{
+//     it(`возведение 2 в степень 3`,()=>{
+//         assert.equal(8,8);
+//     });
+// });
 
 describe(`тестирование функции dowbleElem`,()=>{
-    // let arr=ar;
-    // for(i=0; i<=Ar.length;i++)
-    it(`удвоение элементов массива `, ()=>{
-        assert.equal(2,2);
-    });
+    let ar=Array;
+        it(`проверка тип массив `, ()=>{
+            
+            if(typeof ar===Array){
+            assert.equal(dowbleElem(ar),ar);
+            }
+        });
+        it(`проверка тип элемента массива `, ()=>{
+           
+            for(let i=0; i<=ar.length;i++){
+            if(typeof ar[i]==="number"){
+            assert.equal(dowbleElem(ar),ar);
+            }
+        }
+        });
+
+        
+
 });
 
